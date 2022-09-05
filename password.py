15638bfca4dcb8cbafdc8140984a46b582521fe1
Fiona Xiong <xinyiwanshi1996@163.com>
2022-09-05 15:26:05 +0800

    simple version

--- password.py
+++ password.py
@@ -1,14 +1,13 @@
 password = "a123456"
-num = 3
-user = input ("请输入您的密码：")
-if user == password:
-    print ("登入成功！")
-else:
-    
-    while num > 1:
+num = 3 #剩余机会
+
+while num > 0:
+    user = input ("请输入您的密码：")
+    if user == password:
+        print ("登入成功！")
+        break  #逃出回圈
+    else:
         num = num - 1
         print ("密码错误！还有",num,"次机会")
-        user = input ("请输入您的密码：")
-    while num == 1:
-        print ("密码错误！还有0次机会")
-        break
+        if num == 0:
+            break
